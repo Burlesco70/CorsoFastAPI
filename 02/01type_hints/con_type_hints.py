228a3e999c881fac0f15407fd7fd200e91daea56
Burlesco70 <mario.nardi.70@gmail.com>
2021-05-09 22:17:36 +0200

    modern

---
+++ 02/01type_hints/con_type_hints.py
@@ -0,0 +1,38 @@
+from collections import namedtuple
+from typing import Optional, Iterable
+
+Elemento = namedtuple("Elemento", "voce, prezzo")
+
+costo_massimo: Optional[int] = None
+
+
+def calcola_costo(elementi: Iterable[Elemento]) -> int:
+    global costo_massimo
+    total = 0
+
+    for e in elementi:
+        total += e.prezzo
+
+    if not costo_massimo or total > costo_massimo:
+        costo_massimo = total
+
+    return total
+
+
+def main():
+    print("Inseriamo i pasti consumati nel giorno")
+
+    cena = [Elemento('Pizza', 20), Elemento('Birra', 9), Elemento('Birra', 9)]
+    colazione = [Elemento('Pancakes', 11), Elemento('Bacon', 4), Elemento('Caffè', 3), Elemento('Caffè', 3), Elemento('Brioche', 2)]
+
+    totale_cena = calcola_costo(cena)
+    print(f"Costo cena EUR {totale_cena:,.02f}")
+
+    totale_colazione = calcola_costo(colazione)
+    print(f"Costo colazione EUR {totale_colazione:,.02f}")
+
+    print(f"Oggi il costo più elevato è stato EUR {costo_massimo:.02f}")
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Appunti per approfondire questo promettente framework API
 Trasposizione di:  
 https://github.com/talkpython/modern-apis-with-fastapi
 
+# E dopo?
+
+https://github.com/mjhea0/awesome-fastapi
+
 # Requirements
 
 fastapi
